475417bedb18a8c70095c60c975c1e8f4e3b12cc
Sivamuthu Kumar <sivamuthukumar07@gmail.com>
2020-07-15 12:48:15 -0400

    Add different addons for your storybook

---
+++ magic-lib/.storybook/preview.js
@@ -0,0 +1,10 @@
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json'
+setCompodocJson(docJson);
+
+import { addDecorator } from '@storybook/angular';
+import { withA11y } from '@storybook/addon-a11y';
+
+addDecorator(withA11y);
+
+
